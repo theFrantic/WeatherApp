5fbdad71fa8672233e1cd748c683266791de5a61
Daniel Whilchy <dwhilchy@tycoint.com>
2017-06-07 23:42:10 +0100

    Chapter 7: Anko and Extension Functions
    Changed the way to find the forecast_list element taking advantage of
    Anko library

--- app/src/main/java/com/whilchy/weatherapp/activities/MainActivity.kt
+++ app/src/main/java/com/whilchy/weatherapp/activities/MainActivity.kt
@@ -6,7 +6,7 @@ import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import com.whilchy.weatherapp.R
 import com.whilchy.weatherapp.adapters.ForecastListAdapter
-import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.find
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,7 +24,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val forecastList = findViewById(R.id.forecast_list) as RecyclerView
+        val forecastList: RecyclerView = find(R.id.forecast_list)
         forecastList.layoutManager = LinearLayoutManager(this)
 
         forecastList.adapter = ForecastListAdapter(items)
