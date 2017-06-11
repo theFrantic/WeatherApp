5f5194c45d954f1a1501ec459ea184c2156f3a93
Daniel Whilchy <dwhilchy@tycoint.com>
2017-06-11 14:27:34 +0100

    Chapter 11: Operator overloading
    Operator overloading to simplify the way we retrieve the data

--- app/src/main/java/com/whilchy/weatherapp/domain/model/DomainClasses.kt
+++ app/src/main/java/com/whilchy/weatherapp/domain/model/DomainClasses.kt
@@ -1,9 +1,17 @@
 package com.whilchy.weatherapp.domain.model
 
+import java.text.FieldPosition
+
 /**
  * Created by daniel on 11/06/17.
  */
 
-data class ForecastList(val city: String, val country: String, val dailyForecast: List<Forecast>)
+data class ForecastList(val city: String, val country: String, val dailyForecast: List<Forecast>) {
+
+    val size: Int
+        get() = dailyForecast.size
+
+    operator fun get(position: Int): Forecast = dailyForecast[position]
+}
 
 data class Forecast(val date: String, val description: String, val high: Int, val low: Int)
