6eb52d9e468d9cc97b440729ff4370fde072e939
Daniel Whilchy <dwhilchy@tycoint.com>
2017-06-11 18:22:43 +0100

    Chapter 15: Kotlin Android Extensions
    Taking advantage of the Kotlin Android Extensions to reduce the
    boilerplate code

--- app/src/main/java/com/whilchy/weatherapp/ui/activities/MainActivity.kt
+++ app/src/main/java/com/whilchy/weatherapp/ui/activities/MainActivity.kt
@@ -3,12 +3,11 @@ package com.whilchy.weatherapp.ui.activities
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 import com.whilchy.weatherapp.R
 import com.whilchy.weatherapp.domain.commands.RequestForecastCommand
 import com.whilchy.weatherapp.ui.adapters.ForecastListAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.doAsync
-import org.jetbrains.anko.find
 import org.jetbrains.anko.toast
 import org.jetbrains.anko.uiThread
 
@@ -18,7 +17,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val forecastList: RecyclerView = find(R.id.forecast_list)
         forecastList.layoutManager = LinearLayoutManager(this)
 
         doAsync {
--- app/src/main/java/com/whilchy/weatherapp/ui/adapters/ForecastListAdapter.kt
+++ app/src/main/java/com/whilchy/weatherapp/ui/adapters/ForecastListAdapter.kt
@@ -12,6 +12,7 @@ import com.whilchy.weatherapp.domain.model.Forecast
 import com.whilchy.weatherapp.domain.model.ForecastList
 import com.whilchy.weatherapp.ui.utils.ctx
 import org.jetbrains.anko.find
+import kotlinx.android.synthetic.main.item_forecast.view.*
 
 /**
  * Created by daniel on 07/06/17.
@@ -33,19 +34,13 @@ class ForecastListAdapter(val weekForecast: ForecastList, val itemClick: (Foreca
     class ViewHolder(view: View,
                      val itemClick: (Forecast) -> Unit) : RecyclerView.ViewHolder(view) {
 
-        private val iconView = view.find<ImageView>(R.id.icon)
-        private val dateView = view.find<TextView>(R.id.date)
-        private val descriptionView = view.find<TextView>(R.id.description)
-        private val maxTemperatureView = view.find<TextView>(R.id.maxTemperature)
-        private val minTemperatureView = view.find<TextView>(R.id.minTemperature)
-
         fun bindForecast(forecast: Forecast) {
             with(forecast) {
-                Picasso.with(itemView.ctx).load(iconUrl).into(iconView)
-                dateView.text = date
-                descriptionView.text = description
-                maxTemperatureView.text = "${high}º"
-                minTemperatureView.text = "${low}º"
+                Picasso.with(itemView.ctx).load(iconUrl).into(itemView.icon)
+                itemView.date.text = date
+                itemView.description.text = description
+                itemView.maxTemperature.text = "${high}º"
+                itemView.minTemperature.text = "${low}º"
                 itemView.setOnClickListener { itemClick(this) }
             }
         }
